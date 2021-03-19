bf14f869849706ac3ab20280d26e7bbd1bf844af
DanieleAngioni <angioni.daniele97.00@gmail.com>
2021-03-19 16:25:22 +0100

    Commit per usare la funzione nuova di lettura

--- dataset.py
+++ dataset.py
@@ -2,9 +2,17 @@ from prova_lettura_immagini import read_pgm
 import matplotlib.pyplot as plt
 import os
 
-shape_list = []
-#
-# for img_path in
-# img = read_pgm("DaimlerBenchmark/Data/TrainingData/NonPedestrians/neg00000.pgm", byteorder='<')
+os.chdir(r"C:\Users\angio\PycharmProjects\AI_Project\DaimlerBenchmark\simple_dataset\1")
+path1 = os.getcwd()
+non_ped_path = path1 + r"\non-ped_examples\\"
+ped_path = path1 + r"\ped_examples\\"
+
+for img_path in os.listdir(ped_path):
+
+
+# img_path = "03m_11s_013926u.pgm"
+# img = read_pgm(test_path + img_path, byteorder='<')
 # plt.imshow(img, plt.cm.gray)
 # plt.show()
+
+print("")
